e18228c6a94866e3b102df75cddd9b66de50a203
Vincent Huang <vincom2@gmail.com>
2014-03-09 17:16:46 -0400

    GENERIC QUESTIONS this file contains questions that can be asked of any article, as long as they are classified appropriately.

---
+++ ask/generic.py
@@ -0,0 +1,18 @@
+"""generic questions that can hopefully be asked about any of the articles, as long as we classify them correctly.
+   add your own! https://www.ark.cs.cmu.edu/NLP/S14/project.php"""
+
+soccer_player = [
+    "When was {} born?",
+    "Which club does {} play for?",
+    "When was {}'s first international appearance?"
+]
+constellation = [
+    "What is one star that {} contains?"
+]
+natural_language = [
+    "How many native speakers does {} have?",
+    "What type of language is {}?", #this question is not very good, even among these mediocre ones, imo
+]
+programming_language = [
+    "Is {} usually interpreted or compiled?"
+]
