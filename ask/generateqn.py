0b4dec2c5dc440b53da691cdc07ab368d91a3816
Vincent Huang <vincom2@gmail.com>
2014-03-09 18:17:32 -0400

    nope this doesn't do anything yet

---
+++ ask/generateqn.py
@@ -0,0 +1,9 @@
+"""obviously this doesn't actually do anything yet
+   I just feel like this would be a good way to organise stuff
+   WOOHOO 2 FUNCTIONS"""
+
+def syntactic_inversion():
+    pass
+
+def pick_sentences():
+    pass
