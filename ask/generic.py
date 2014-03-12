b0f1e5c793804d887bb0bd810cd1ca9443efe63e
Vincent Huang <vincom2@gmail.com>
2014-03-12 00:22:04 -0400

    more stuff to do

--- ask/generic.py
+++ ask/generic.py
@@ -1,6 +1,13 @@
 """generic questions that can hopefully be asked about any of the articles, as long as we classify them correctly.
    add your own! https://www.ark.cs.cmu.edu/NLP/S14/project.php"""
 
+# perhaps each generic question should be a tuple of (generic qn, "dependencies") instead
+# where dependencies would be a list of words that really should occur somewhere in the article
+# before we bring out that question.
+# for instance, there might be a shitty footballer who's never had an international appearance,
+# and we might not want to use the "first international appearance" question then (even though
+# it's still conceivably a valid question, it seems kind of weird).
+
 soccer_player = [
     "When was {} born?",
     "Which club does {} play for?",
