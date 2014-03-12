6c73e8827f64648acfa9fca3345668826ef923df
Vincent Huang <vincom2@gmail.com>
2014-03-12 00:16:25 -0400

    reorganized classifier stuff classify.py now actually classifies an article you give it, using the pickled classifier at data/classifier.pickle the load time is horrendous. we should figure out a way around it.

---
+++ ask/classify.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python
+
+import os.path
+import nltk
+import pickle
+import sys
+from train_classifier import word_feats
+
+classifier_loc = "../data/classifier.pickle"
+
+def load_article(article):
+    with open(article) as f:
+        input = f.read()
+
+    return word_feats(nltk.word_tokenize(input))
+
+def classify(article):
+    with open(classifier_loc) as f:
+        classifier = pickle.load(f)
+
+    return classifier.classify(load_article(article))
+
+
+def main():
+    if len(sys.argv) != 2:
+        print "usage: ./classify.py <classifythis.txt>"
+        sys.exit(1)
+
+    # this is horrendously slow and we probably need a good way to ameliorate it
+    # classify large batches at a go?
+    # or maybe a binary format will load faster
+    # INVESTIGATE
+    with open(classifier_loc) as f:
+        classifier = pickle.load(f)
+
+    with open(sys.argv[1]) as f:
+        input = f.read()
+
+    data = word_feats(nltk.word_tokenize(input))
+
+    print classifier.classify(data)
+
+if __name__ == '__main__':
+    main()
--- ask/train_classifier.py
+++ ask/train_classifier.py
@@ -14,7 +14,6 @@ from nltk.corpus.reader import CategorizedPlaintextCorpusReader
 import pickle
 
 corpusdir = '../data/own'
-articles = CategorizedPlaintextCorpusReader(corpusdir, '.*', cat_pattern = r'(.*)[/]')
 
 def word_feats(words):
     return dict([(word, True) for word in words])
@@ -25,32 +24,34 @@ def hold_back(len):
     else:
         return len/4
 
-feats = {}
-trainfeats = []
-testfeats = []
-for cat in articles.categories():
-    wow = len([f for f in articles.fileids(cat)]) # such variable name
-    print "for category", cat, ":", wow
-    feats[cat] = [(word_feats(articles.words(fileids = [f])), cat) for f in articles.fileids(cat)]
-    cutoff = wow - hold_back(wow)
-    trainfeats.append(feats[cat][:cutoff])
-    testfeats.append(feats[cat][cutoff:])
+def main():
+    articles = CategorizedPlaintextCorpusReader(corpusdir, '.*', cat_pattern = r'(.*)[/]')
+    feats = {}
+    trainfeats = []
+    testfeats = []
+    for cat in articles.categories():
+        wow = len([f for f in articles.fileids(cat)]) # such variable name
+        print "for category", cat, ":", wow
+        feats[cat] = [(word_feats(articles.words(fileids = [f])), cat) for f in articles.fileids(cat)]
+        cutoff = wow - hold_back(wow)
+        trainfeats.append(feats[cat][:cutoff])
+        testfeats.append(feats[cat][cutoff:])
 
-train = [item for sublist in trainfeats for item in sublist]
-test = [item for sublist in testfeats for item in sublist]
+    train = [item for sublist in trainfeats for item in sublist]
+    test = [item for sublist in testfeats for item in sublist]
 
-print 'train on %d instances, test on %d instances' % (len(train), len(test))
+    print 'train on %d instances, test on %d instances' % (len(train), len(test))
 
-classifier = NaiveBayesClassifier.train(train)
-print 'accuracy:', nltk.classify.util.accuracy(classifier, test)
-classifier.show_most_informative_features() # I don't understand the output for more than 2 categories :(
+    classifier = NaiveBayesClassifier.train(train)
+    print 'accuracy:', nltk.classify.util.accuracy(classifier, test)
+    classifier.show_most_informative_features() # I don't understand the output for more than 2 categories :(
 
-# load with:
-# import pickle
-# f = open('my_classifier.pickle')
-# classifier = pickle.load(f)
-# f.close()
-with open('../data/classifier.pickle', 'wb') as f:
-    pickle.dump(classifier, f)
-    # wtf it's 50MB
-    # use cPickle or import gzip or import bz2 or something
+    # load with:
+    # import pickle
+    # f = open('my_classifier.pickle')
+    # classifier = pickle.load(f)
+    # f.close()
+    with open('../data/classifier.pickle', 'wb') as f:
+        pickle.dump(classifier, f)
+        # wtf it's 50MB
+        # use cPickle or import gzip or import bz2 or something
